4688154879e7ed0201b09f9ec2f98c9856d74057
SamShapiro <samuel.shapiro123@gmail.com>
2020-12-03 17:31:33 -0500

    modified entry types, added functionality to EntryViewModel

--- ProjectCODYMentorApp/app/src/main/java/com/example/projectcodymentorapp/EntryViewModel.kt
+++ ProjectCODYMentorApp/app/src/main/java/com/example/projectcodymentorapp/EntryViewModel.kt
@@ -18,23 +18,65 @@ class EntryViewModel : ViewModel(), ValueEventListener {
     init {
         entryList.value = ArrayList()
         firebaseDatabase.value = Firebase.database.getReference("")
+        firebaseDatabase.value?.addValueEventListener(this)
     }
 
     /**
      * get all students
      */
-    fun getStudents() {
-
+    fun getStudents(): ArrayList<Student> {
+        val students = ArrayList<Student>()
+        entryList.value?.forEach {
+            if (it is Student) {
+                students.add(it)
+            }
+        }
+        return students
     }
 
     fun setCurrentEntry(entry: Entry) {
+        currentEntry.postValue(entry);
+    }
+
+    fun addOrUpdateEntry(entry: Entry) {
+        var id = "id"
+        var pathName = "type"
+        if (entry is Student) {
+            id = entry.email.replace(".", "")
+            pathName = "Students"
+        }
+        else if (entry is Mentor) {
+            id = entry.email.replace(".", "")
+            pathName = "Mentors"
+        }
+        else if (entry is Course) {
+            id = entry.name.replace(" ", "_")
+            pathName = "Courses"
+        }
+
+        firebaseDatabase.value?.child(pathName)?.child(id)?.setValue(entry)
 
     }
 
     override fun onDataChange(snapshot: DataSnapshot) {
         // Get everything in the database and postValue
         val entries = ArrayList<Entry>()
-        snapshot.children
+        snapshot.child("Students").children.forEach {
+            it.getValue(Student::class.java)?.let {
+                entries.add(it)
+            }
+        }
+        snapshot.child("Courses").children.forEach {
+            it.getValue(Course::class.java)?.let {
+                entries.add(it)
+            }
+        }
+        snapshot.child("Mentors").children.forEach {
+            it.getValue(Mentor::class.java)?.let {
+                entries.add(it)
+            }
+        }
+        entryList.postValue(entries)
     }
 
     override fun onCancelled(error: DatabaseError) {
---
+++ ProjectCODYMentorApp/app/src/main/java/com/example/projectcodymentorapp/Mentor.kt
@@ -0,0 +1,24 @@
+package com.example.projectcodymentorapp
+
+class Mentor () : Entry() {
+    var name: String = "name"
+    var email: String = "email@example.com"
+    var session1_mw: String = "yes"
+    var session1_tr: String = "yes"
+    var session2_mw: String = "yes"
+    var session2_tr: String = "yes"
+    var friday_dropin: String = "yes"
+    var arts_anim: String = "5"
+    var game_dev: String = "5"
+    var compsci: String = "5"
+    var web_design: String = "5"
+    var app_dev: String = "5"
+    var number_of_courses: String = "1"
+    var young_or_old: String = "young"
+    var orientation: String = "any time"
+    var website_name: String = "First Last"
+    var website_photo: String = "link"
+    var website_desc: String = "cool person"
+    var website_personal: String = "yes"
+    var comments: String = "none"
+}
